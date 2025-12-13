1215953443ce11d2932ba6acffaada0820e916a9
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-12-14 03:54:08 +0900

    fix: change icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { AnalyzeResult } from "@lycorp-jp/tappy";
-import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { CrossCircledIcon, ExternalLinkIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 import useSWRMutation from "swr/mutation";
 import { AnalyzeOverlay } from "@/components/analyze-overlay";
@@ -48,19 +48,7 @@ export default function Home() {
       {errorMessage && (
         <div className="toast toast-top toast-end z-50">
           <div className="alert alert-error rounded-sm border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+            <CrossCircledIcon />
             <span>{errorMessage}</span>
           </div>
         </div>
